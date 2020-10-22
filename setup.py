084422adb7d8a7430ba44b0c14c4073d42f8fc7d
maze508 <ac0bge@gmail.com>
2020-10-22 18:09:33 +0800

    Added requirements .txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Jiaweis_API',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Admin',
+    author_email='',
+    description=''
+)
