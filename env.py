317ce5ac2f668dedf149edc4ebdccaa2ee85c30d
maze508 <72195604+maze508@users.noreply.github.com>
2021-01-06 18:23:09 +0800

    Update env.py
    Sensitive Information Masked

--- env.py
+++ env.py
@@ -2,10 +2,10 @@
 PATH_TO_CHROME_DRIVER = r"C:\Users\Admin\Documents\chromedriver.exe"
 
 # Link to the Strava Leaderboards website 
-LEADERBOARDS_WEBSITE = 'https://www.strava.com/clubs/midswing'
+LEADERBOARDS_WEBSITE = 'https://www.strava.com/clubs/``` Club Name Here ```'
 
 # Location of the Backup CSV and its name
-LOCATION_TO_DOWNLOAD_CSV = r"C:\Users\Admin\Desktop\Work\Jiaweis_API\strava_leaderboards.csv"
+LOCATION_TO_DOWNLOAD_CSV = r" ``` Location to Download CSV ```"
 
 # Interval in which you want to run the script
 UPLOAD_TO_GOOGLE_SHEETS_INTERVAL_MINUTES = 30
